eee8d548820de770feb3b9b3ca9fee2e4effabad
MaliqYz <maliqyz1801@gmail.com>
2024-06-22 16:39:37 +0800

    UPDATE

--- myuts/myuts/settings.py
+++ myuts/myuts/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4m1f-v0p8baon6*h9#9_iorwq%_w1&gad^cl80i^_b1^g*0nue"
+# SECRET_KEY = "django-insecure-4m1f-v0p8baon6*h9#9_iorwq%_w1&gad^cl80i^_b1^g*0nue"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
